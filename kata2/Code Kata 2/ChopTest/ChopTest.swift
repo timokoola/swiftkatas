a7ef80cc4640037d171e2f70d9ffc4ec45f6f8d4
Timo Koola <timo.koola@gmail.com>
2019-12-08 21:01:49 +0200

    all kata tests in

--- kata2/Code Kata 2/ChopTest/ChopTest.swift
+++ kata2/Code Kata 2/ChopTest/ChopTest.swift
@@ -9,15 +9,15 @@
 import XCTest
 
 class ChopTest: XCTestCase {
-
+    
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testChopEmpty() {
         // GIVEN
         let testArr : [Int] = []
@@ -28,5 +28,31 @@ class ChopTest: XCTestCase {
         // THEN
         XCTAssertEqual(-1, result)
     }
- 
+    
+    func testAll() {
+        // G&W&T
+        XCTAssertEqual(-1, dummyChop(3, array: []))
+        XCTAssertEqual(-1, dummyChop(3, array: [1]))
+        XCTAssertEqual(0,  dummyChop(1, array: [1]))
+        //
+        XCTAssertEqual(0,  dummyChop(1, array: [1, 3, 5]))
+        XCTAssertEqual(1,  dummyChop(3, array: [1, 3, 5]))
+        XCTAssertEqual(2,  dummyChop(5, array: [1, 3, 5]))
+        XCTAssertEqual(-1, dummyChop(0, array: [1, 3, 5]))
+        XCTAssertEqual(-1, dummyChop(2, array: [1, 3, 5]))
+        XCTAssertEqual(-1, dummyChop(4, array: [1, 3, 5]))
+        XCTAssertEqual(-1, dummyChop(6, array: [1, 3, 5]))
+        //
+        XCTAssertEqual(0,  dummyChop(1, array: [1, 3, 5, 7]))
+        XCTAssertEqual(1,  dummyChop(3, array: [1, 3, 5, 7]))
+        XCTAssertEqual(2,  dummyChop(5, array: [1, 3, 5, 7]))
+        XCTAssertEqual(3,  dummyChop(7, array: [1, 3, 5, 7]))
+        XCTAssertEqual(-1, dummyChop(0, array: [1, 3, 5, 7]))
+        XCTAssertEqual(-1, dummyChop(2, array: [1, 3, 5, 7]))
+        XCTAssertEqual(-1, dummyChop(4, array: [1, 3, 5, 7]))
+        XCTAssertEqual(-1, dummyChop(6, array: [1, 3, 5, 7]))
+        XCTAssertEqual(-1, dummyChop(8, array: [1, 3, 5, 7]))
+    }
+    
+    
 }
